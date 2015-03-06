11904341858106b55b4fb99551758de2b11ff8a4
brandontate <blt900@gmail.com>
2015-03-06 14:40:51 -0600

    Removed default Routing and Page Info to the Todo Page and Controller

---
+++ TodoApp/Controllers/TodoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TodoApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        // GET: Todo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Description";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Brandon Tate";
+
+            return View();
+        }
+    }
+}
